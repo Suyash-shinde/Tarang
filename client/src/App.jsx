70cfcde0757e6f101461b0d8f538d64bef6bd195
Suyash-shinde <suyash.c.shinde@gmail.com>
2024-03-09 22:07:31 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import Login from "./components/Login";
+
 import "./App.css";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />}/>
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
