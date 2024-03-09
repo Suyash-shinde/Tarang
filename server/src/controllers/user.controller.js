007bbce3d5a57edf208ef6a5be1954cf561943c8
Suyash-shinde <suyash.c.shinde@gmail.com>
2024-03-09 21:49:58 +0530

    ..

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,12 +1,21 @@
 import { User } from "../models/user.model.js";
-
+import bcrypt from "bcrypt";
 export const Register = async(req,res,next)=>{
     try {
     const{ firstname, lastname,password,email,gender, mobile, volunteertype, timeavailability} =  req.body;
+    const findMobile= User.findOne({mobile});
+    const findEmail= User.findOne({email});
+    if(findMobile){
+        return res.json({msg:"Mobile Number already in use", status:false});
+    }
+    if(findEmail){
+        return res.json({msg:"Email already in use", status:false});
+    }
+    const encryptedPassword=bcrypt.hashSync(password,10);
     const send = await User.create({
         firstname,
         lastname,
-        password,
+        password:encryptedPassword,
         email,
         gender,
         mobile,
