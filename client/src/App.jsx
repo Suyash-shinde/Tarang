8d7d58d0bfe858431d011979b4461a0e18c5d1c5
Egon15 <pradneshtilekar15@gmail.com>
2024-03-12 22:31:52 +0530

    Updated Profile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import SignUp from "./components/SignUp";
 import "./App.css";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
     <>
       <SignUp />
+      <Profile />
     </>
   );
 }
