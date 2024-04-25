360366b9fffa1119591ee1ca631586ee11440148
Sidh Jain <sidhj211@gmail.com>
2024-04-26 01:19:47 +0530

    Changed Event page css and stuff

--- client/src/components/EventPage.jsx
+++ client/src/components/EventPage.jsx
@@ -1,41 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import { getEventDetail } from '../utils/Api.post';
-import Navbar from './Navbar'
+import { Row, Col, Image, Card, Button, ListGroup, ListGroupItem, Container } from 'react-bootstrap';
+
+import Navbar from './Navbar';
 import { ParticipatePage } from './ParticipatePage';
+
 export const EventPage = () => {
-    const [Details,setDetails]=useState({});
-    const{id:eventId}= useParams();
-    const navigate= useNavigate();
-    const getDetails= async()=>{
-        const {data} = await getEventDetail(eventId);
-        console.log(data) ;
-        setDetails((prev)=>data);
-        
-    }
-    useEffect(()=>{
+    const [Details, setDetails] = useState({});
+    const { id: eventId } = useParams();
+    const navigate = useNavigate();
+
+    const getDetails = async () => {
+        try {
+            const { data } = await getEventDetail(eventId);
+            console.log(data);
+            setDetails(data);
+        } catch (error) {
+            console.error('Error fetching event details:', error);
+        }
+    };
+
+    useEffect(() => {
         getDetails();
-        
-    },[eventId]);
-    const handleParticipate=()=>{
-      navigate('/participatepage', {state:{details:Details}});
-    }
-  return (
-    <>
-    <Navbar/>
-    <div>
-    {Details.title} 
-    </div>
-    <div>
-    {Details.location}
-    </div>
-    <div>
-    {Details.description}
-    </div>
-    <div>
-      {Details.date}
-    </div>
-    <button onClick={handleParticipate}>Participate</button>
-    </>
-  )
-}
+    }, [eventId]);
+
+    const handleParticipate = () => {
+        navigate('/participatepage', { state: { details: Details } });
+    };
+
+    return (
+        <>
+            <Navbar />
+            <Container className="d-flex justify-content-center align-items-center" style={{ minHeight: '50vh' }}>
+                <div>
+                    <Row>
+                        <Col md={5}>
+                            <Image src="../public/home/ngo2.jpeg" fluid />
+                        </Col>
+                        <Col md={4}>
+                            <ListGroup variant="flush">
+                                <ListGroupItem>
+                                    <h3>{Details.title}</h3>
+                                </ListGroupItem>
+                                <ListGroupItem>Description {Details.description}</ListGroupItem>
+                            </ListGroup>
+                        </Col>
+                        <Col md={3}>
+                            <Card>
+                                <ListGroup variant="flush">
+                                    <ListGroupItem>
+                                        <Row>
+                                            <Col>Date:</Col>
+                                            <Col>
+                                                <strong>{Details.date}</strong>
+                                            </Col>
+                                        </Row>
+                                    </ListGroupItem>
+                                    <ListGroupItem>
+                                        <Row>
+                                            <Col>Location: {Details.location}</Col>
+                                        </Row>
+                                    </ListGroupItem>
+                                    <ListGroupItem className="d-flex justify-content-center">
+                                        <Button className="btn-block" type="button">
+                                            Participate
+                                        </Button>
+                                    </ListGroupItem>
+                                </ListGroup>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            </Container>
+        </>
+    );
+};
