a2463d0e467c9540a5af5e03a15a095a589d72b1
Chinmay Wadhe <chinmaywadhe26@gmail.com>
2024-04-26 07:35:22 +0530

    .

--- client/src/components/VCSS.jsx
+++ client/src/components/VCSS.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { Card, CardBody, CardText } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Card, CardBody, CardText } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
-const VCSS = ({event}) => {
-  const navigate=useNavigate()
+const VCSS = ({ event }) => {
+  const navigate = useNavigate();
   return (
-    <Card key={event._id} onClick={()=>{
-      navigate(`/event/${event._id}`)
-    }} className='my-3 p-3 rounded'>
-    <CardBody >
-<<<<<<< HEAD
-        
-            <Card.Title as='div'className='product-title' >
-=======
-    <Card.Img src="../public/home/ngo2.jpeg" variant="top"/>
+    <Card
+      key={event._id}
+      onClick={() => {
+        navigate(`/event/${event._id}`);
+      }}
+      className="my-3 p-3 rounded"
+    >
+      <CardBody>
+        <Card.Img src="../public/home/ngo2.jpeg" variant="top" />
 
-            <Card.Title as='div' className='product-title d-flex justify-content-center align-items-center'>
->>>>>>> b087ecef2d2912c65c5cb0123e13edc09ad12473
-              {event.title}
-                </Card.Title>
-            
-     </CardBody>  
+        <Card.Title
+          as="div"
+          className="product-title d-flex justify-content-center align-items-center"
+        >
+          {event.title}
+        </Card.Title>
+      </CardBody>
     </Card>
-    
-  )
-}
+  );
+};
 
-export default VCSS
+export default VCSS;
